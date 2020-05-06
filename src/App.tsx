4143bde4c81e0b62001f7b254a9b0a0aa158f952
Ash <ashnamuh@gmail.com>
2020-05-06 13:00:36 +0900

    mark-up ashnamuh-blog page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import 'styles/global.scss'
 import IndexPage from 'pages/index'
 import AshPage from 'pages/ash'
 import HCenterPage from 'pages/horizontal-center'
+import AshnamuhBlogPage from 'pages/ashnamuh-blog'
 
 export default function App() {
   return (
@@ -29,12 +30,16 @@ export default function App() {
               <li>
                 <Link to="/horizontal-center">HCenter</Link>
               </li>
+              <li>
+                <Link to="/ashnamuh-blog">Ashnamuh blog</Link>
+              </li>
             </ul>
 
             <Switch>
               <Route exact path="/" component={IndexPage} />
               <Route exact path="/ash" component={AshPage} />
               <Route exact path="/horizontal-center" component={HCenterPage} />
+              <Route exact path="/ashnamuh-blog" component={AshnamuhBlogPage} />
             </Switch>
           </div>
         </Router>
---
+++ src/pages/ashnamuh-blog.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react'
+import styles from './ashnamuh-blog.module.scss'
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <aside className={styles.aside}>
+          <h1>ashnamuh</h1>
+          <nav>
+            <ul>
+              <li>category1</li>
+              <li>category2</li>
+              <li>category3</li>
+              <li>category4</li>
+              <li>category5</li>
+            </ul>
+          </nav>
+        </aside>
+        <main className={styles.main}>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+          <article className={styles.article}>
+            <time dateTime="2020-05-06">May 2020</time><span>category</span>
+            <h2>Title of a post</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a>Read</a>
+          </article>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default Page
